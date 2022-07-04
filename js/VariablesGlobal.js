b7eb6465f10994ef5fcc4bfb8c1f5e41b7d17037
Cristhian Joel Gomez Jarquin <cristhjar.dev@gmail.com>
2022-07-03 22:55:37 -0600

    Cambios en la variables Globales

---
+++ js/VariablesGlobal.js
@@ -0,0 +1,56 @@
+export var Banner_Principal = [{ Link: "youtube", id: 1 }];
+
+var AllProducts = [
+  {
+    Link: "https://i-cf65.ch-static.com/content/dam/cf-consumer-healthcare/bp-advil/es_US/products/thumbnail_below_toute_032917.jpg?auto=format",
+    Titulo: "Contra El dolor de cabeza",
+    Descuento: "16%",
+    Precio: "567",
+    ConDescuento: "440",
+    Lab: "Advil",
+    Caracteristicas: [
+      "Eficaz",
+      "Acaba De manera Rapida EL Dolor de cabes",
+      "Nada Mejor",
+    ],
+  },
+  {
+    Link: "https://www.gaceta.unam.mx/wp-content/uploads/2020/09/200907-aca1-des-f1-farmacos-con-base-en-organismos-marinos.jpg",
+    Titulo: "Genericos Contra EL dolor",
+    Descuento: "16%",
+    Precio: "430",
+    ConDescuento: "440",
+    Lab: "Advil",
+    Caracteristicas: [
+      "Accesibles",
+      "Importadas",
+      "De excelente Calidad",
+    ],
+  },
+  {
+    Link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI65bE37s8AGn7Fglbpb5R8LUVkxMgfwnFdA&usqp=CAU",
+    Titulo: "Desinflama y activa Tu Sistema",
+    Descuento: "16%",
+    Precio: "567",
+    ConDescuento: "440",
+    Lab: "Advil",
+    Caracteristicas: [
+      "Accion rapida",
+      "Lo Mejor",
+      "Intensivo",
+    ],
+  },
+  {
+    Link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRuWD5aguDe8NYilE-z3SNHkPxc7FonI6u2A&usqp=CAU",
+    Titulo: "EL Maletar Adios",
+    Descuento: "16%",
+    Precio: "230",
+    ConDescuento: "440",
+    Lab: "Advil",
+    Caracteristicas: [
+      "Dile adios Rapido",
+      "Ya no Sufars",
+      "Aun Esperas",
+    ],
+  }
+];
